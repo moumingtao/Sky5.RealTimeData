53a69c5b6330d28665ccc88cf4b5d60cdbfbc2ca
MingTao <3073429849@qq.cm>
2020-06-09 08:53:59 +0800

    添加少量Filter实现

--- Sky5.RealTimeData.Test/UnitTest1.cs
+++ Sky5.RealTimeData.Test/UnitTest1.cs
@@ -14,5 +14,21 @@ namespace Sky5.RealTimeData.Test
             var filter = Builders<BsonDocument>.Filter.Eq("name", "Tom");
             var doc1 = BsonDocument.Parse("{'name', '233'}");
         }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var array1 = new BsonArray(new int[] { 1, 2, 3 });
+            var array2 = new BsonArray(new int[] { 1, 2, 3 });
+            Assert.IsTrue(array1.Equals(array2));
+            new BsonInt32(1).Equals(array1);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var doc = new { Name = (string)null }.ToBsonDocument();
+        }
+
     }
 }
--- Sky5.RealTimeData/Logic/FilterUtils.cs
+++ Sky5.RealTimeData/Logic/FilterUtils.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace Sky5.RealTimeData.Logic
 {
+    // https://docs.mongodb.com/manual/reference/operator/query/
     public class FilterUtils
     {
         static readonly Dictionary<string, Func<BsonDocument, BsonDocument, bool>> methods = new Dictionary<string, Func<BsonDocument, BsonDocument, bool>>();
@@ -16,15 +17,54 @@ namespace Sky5.RealTimeData.Logic
         }
         static void Register(Func<BsonDocument, BsonDocument, bool> method) => methods.Add(method.Method.Name, method);
 
-        public static bool IsMatch(BsonDocument filter, BsonDocument value)
+        public static bool IsMatch(BsonDocument filter, BsonValue value)
         {
             if (filter == null) return true;
             return and(filter, value);
         }
 
-        static bool and(BsonValue filter, BsonDocument value)
+        // { <field>: { $eq: <value> } }
+        static bool eq(BsonValue filter, BsonValue value)
         {
-            if(filter is BsonArray array)
+            if (object.Equals(filter, value)) return true;
+            if (value is BsonArray array)
+                return array.Contains(value);
+            return false;
         }
+        // {field: {$gt: value} }
+        static bool gt(BsonValue filter, BsonValue value) => filter > value;
+        static bool gte(BsonValue filter, BsonValue value) => filter >= value;
+        static bool @in(BsonValue filter, BsonValue value)
+        {
+            var array = (BsonArray)filter;
+            if (value == null) return false;
+            foreach (var item in array)
+            {
+                if (Equals(item, value)) return true;
+                if (item is BsonRegularExpression reg && reg.ToRegex().IsMatch(value.AsString))
+                    return true;
+            }
+            return false;
+        }
+        static bool lt(BsonValue filter, BsonValue value) => filter < value;
+        static bool lte(BsonValue filter, BsonValue value) => filter <= value;
+        static bool ne(BsonValue filter, BsonValue value) => filter != value;
+        static bool nin(BsonValue filter, BsonValue value)
+        {
+            var array = (BsonArray)filter;
+            if (value == null) return true;
+            foreach (var item in array)
+                if (Equals(item, value))
+                    return false;
+            return true;
+        }
+
+        static bool and(BsonValue filter, BsonValue value)
+        {
+            foreach (BsonDocument item in (BsonArray)filter)
+                if (!IsMatch(item, value)) return false;
+            return true;
+        }
+
     }
 }
